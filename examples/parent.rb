caa20194378a1f7cdcea14b7a87cb530d498588a
monkeymind <monkeymind@0213238c-dfba-41f1-96e9-6bf741841a8f>
2008-04-10 17:01:09 +0000

    added parent example to show Doodle.parent in use
    git-svn-id: svn+ssh://rubyforge.org/var/svn/doodle@56 0213238c-dfba-41f1-96e9-6bf741841a8f

---
+++ examples/parent.rb
@@ -0,0 +1,40 @@
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'doodle'
+require 'yaml'
+
+class Child < Doodle::Base
+  has :name
+  has :dad do
+    # I'm treating block arguments and Proc object (proc/lambda) arguments
+    # to :init differently:
+    # - a proc/lamba is treated as a literal argument, i.e. the
+    # - value is set to a Proc
+    # - a block argument, on the other hand, is instance
+    # - evaluated during initialization
+    # - consequences
+    #   - can only be done in init block
+    #   - somewhat subtle difference (from programmer's point of
+    #   - view) between a proc and a block
+    
+    # Also note re: Doodle.parent - its value is only valid
+    # during initialization - this is a way to capture that
+    # value for ues later
+    
+    init do
+      Doodle.parent
+    end
+  end
+end
+
+class Parent < Child
+  has :children, :collect => Child
+end
+
+parent = Parent 'Conn' do
+  child 'Sean'
+end
+
+puts parent.to_yaml
+
+
+
