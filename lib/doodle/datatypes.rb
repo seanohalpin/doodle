365f18863b289f3c6fc1235fa9d468da441565dd
monkeymind <monkeymind@0213238c-dfba-41f1-96e9-6bf741841a8f>
2008-04-16 22:33:34 +0000

    added lib/doodle directory and moved datatypes there
    git-svn-id: svn+ssh://rubyforge.org/var/svn/doodle@84 0213238c-dfba-41f1-96e9-6bf741841a8f

--- lib/doodle/datatypes.rb
+++ lib/doodle/datatypes.rb
@@ -1,6 +1,3 @@
-$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$:.unshift(File.join(File.dirname(__FILE__), '.'))
-
 require 'doodle'
 require 'date'
 require 'uri'
@@ -140,51 +137,3 @@ end
 # tell doodle to incorporate the datatypes
 Doodle.datatypes DataTypes
 
-if __FILE__ == $0
-
-  class DateRange < Doodle::Base
-    doodle do
-      date :start
-      date :end do
-        default { start + 1 }
-      end
-      version :version, :default => "0.0.1"
-    end
-  end
-
-  #pp DateRange.instance_methods(false)
-
-  class Person < Doodle::Base
-    doodle do
-      #    string :name, :max => 10
-      name :name, :size => 3..10
-      integer :age
-      email :email, :default => ''
-    end
-  end
-
-  def try(&block)
-    begin
-      block.call
-    rescue Exception => e
-      e
-    end
-  end
-
-  require 'pp'
-
-  pp try { DateRange "2007-01-18", :version => [0,0,9] }
-  pp try { Person 'Sean', '45', 'someone@example.com' }
-  pp try { Person 'Sean', '45' }
-  pp try { Person 'Sean', 'old' }
-  pp try { Person 'Sean', 45, 'this is not an email address' }
-  pp try { Person 'This name is too long', 45 }
-  pp try { Person 'Sean', 45, 42 }
-  pp try { Person 'A', 45 }
-  pp try { Person '123', 45 }
-  pp try { Person '', 45 }
-#   pp try {
-#     person = Person 'Sean', 45
-#     person.name.silly
-#   }
-end
