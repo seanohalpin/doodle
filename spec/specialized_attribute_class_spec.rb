c5e333dfe02e7d4985eb5239979cd44c8cad0726
Sean O'Halpin <sean.ohalpin@gmail.com>
2008-05-05 04:25:22 +0100

    spec for passing attribute name as named param

--- spec/specialized_attribute_class_spec.rb
+++ spec/specialized_attribute_class_spec.rb
@@ -1,5 +1,7 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
+require 'doodle/datatypes'
+
 describe 'Doodle', 'specialized attributes' do
   temporary_constant :Foo, :SpecializedAttribute do
     before :each do
@@ -57,7 +59,53 @@ describe 'Doodle', 'specialized attributes' do
       foo = Foo.new('hi')
       foo.attributes[:ivar1].flag.should_be "sflag"
     end
-    
+
+    it 'should allow using datatypes in additional directives invoking specialized attribute of correct class' do
+      class SpecializedAttribute
+        doodle do
+          string :flag, :max => 1
+        end
+      end
+      class Foo < Doodle
+        class << self
+          def option(*args, &block)
+            # this is how to add extra options onto args array for has
+            # - all hashes get merged into one
+            args << { :using => SpecializedAttribute }
+            has(*args, &block)
+          end
+        end
+        rv = option(:ivar1, :kind => String, :flag => "x")
+        rv.class.should_be SpecializedAttribute
+        rv.flag.should_be 'x'
+      end
+      Foo.attributes[:ivar1].flag.should_be "x"
+      foo = Foo.new('hi')
+      foo.attributes[:ivar1].flag.should_be "x"
+    end
+
+    it 'should allow using datatypes in additional directives invoking specialized attribute of correct class and raise error if incorrect value supplied' do
+      class SpecializedAttribute
+        doodle do
+          string :flag, :max => 1
+        end
+      end
+      class Foo < Doodle
+        class << self
+          def option(*args, &block)
+            args << { :using => SpecializedAttribute }
+            has(*args, &block)
+          end
+        end
+      end
+      proc { Foo.class_eval { option(:ivar1, :kind => String, :flag => "ab") }}.should raise_error(Doodle::ValidationError)
+    end
+
+    it 'should allow specifying name as named parameter' do
+      class Foo < Doodle
+        has :name => :ivar
+      end
+    end
   end
 end
 
