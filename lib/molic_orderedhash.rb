b5d0e5d03390acc314697b2056958c48a2a479fa
monkeymind <monkeymind@0213238c-dfba-41f1-96e9-6bf741841a8f>
2008-08-12 23:18:37 +0000

    removed folding comments
    git-svn-id: svn+ssh://rubyforge.org/var/svn/doodle@221 0213238c-dfba-41f1-96e9-6bf741841a8f

--- lib/molic_orderedhash.rb
+++ lib/molic_orderedhash.rb
@@ -8,208 +8,145 @@
 # THANKS
 #    Andrew Johnson for his suggestions and fixes of Hash[],
 #    merge, to_a, inspect and shift
-class OrderedHash < ::Hash
-#--{{{
+class Doodle
+  class OrderedHash < ::Hash
     attr_accessor :order
 
     class << self
-#--{{{
-        def [] *args
-#--{{{
-            hsh = OrderedHash.new
-            if Hash === args[0]
-                hsh.replace args[0]
-            elsif (args.size % 2) != 0
-                raise ArgumentError, "odd number of elements for Hash"
-            else
-                hsh[args.shift] = args.shift while args.size > 0
-            end
-            hsh
-#--}}}
+      def [] *args
+        hsh = OrderedHash.new
+        if Hash === args[0]
+          hsh.replace args[0]
+        elsif (args.size % 2) != 0
+          raise ArgumentError, "odd number of elements for Hash"
+        else
+          hsh[args.shift] = args.shift while args.size > 0
         end
-#--}}}
+        hsh
+      end
     end
-#        def initialize 
-##--{{{
-#            @order = []
-##--}}}
-#        end
     def initialize(*a, &b)
-#--{{{
       super
       @order = []
-#--}}}
     end
     def store_only a,b
-#--{{{
-        store a,b
-#--}}}
+      store a,b
     end
     alias orig_store store    
     def store a,b
-#--{{{
-        @order.push a unless has_key? a
-        super a,b
-#--}}}
+      @order.push a unless has_key? a
+      super a,b
     end
     alias []= store
     def == hsh2
-#--{{{
-        return false if @order != hsh2.order
-        super hsh2
-#--}}}
+      return false if @order != hsh2.order
+      super hsh2
     end
     def clear
-#--{{{
-        @order = []
-        super
-#--}}}
+      @order = []
+      super
     end
     def delete key
-#--{{{
-        @order.delete key
-        super
-#--}}}
+      @order.delete key
+      super
     end
     def each_key
-#--{{{
-        @order.each { |k| yield k }
-        self
-#--}}}
+      @order.each { |k| yield k }
+      self
     end
     def each_value
-#--{{{
-        @order.each { |k| yield self[k] }
-        self
-#--}}}
+      @order.each { |k| yield self[k] }
+      self
     end
     def each
-#--{{{
-        @order.each { |k| yield k,self[k] }
-        self
-#--}}}
+      @order.each { |k| yield k,self[k] }
+      self
     end
     alias each_pair each    
     def delete_if
-#--{{{
-        @order.clone.each { |k| 
-            delete k if yield
-        }
-        self
-#--}}}
+      @order.clone.each { |k| 
+        delete k if yield
+      }
+      self
     end
     def values
-#--{{{
-        ary = []
-        @order.each { |k| ary.push self[k] }
-        ary
-#--}}}
+      ary = []
+      @order.each { |k| ary.push self[k] }
+      ary
     end
     def keys
-#--{{{
-        @order
-#--}}}
+      @order
     end
     def invert
-#--{{{
-        hsh2 = Hash.new    
-        @order.each { |k| hsh2[self[k]] = k }
-        hsh2
-#--}}}
+      hsh2 = Hash.new    
+      @order.each { |k| hsh2[self[k]] = k }
+      hsh2
     end
     def reject &block
-#--{{{
-        self.dup.delete_if(&block)
-#--}}}
+      self.dup.delete_if(&block)
     end
     def reject! &block
-#--{{{
-        hsh2 = reject(&block)
-        self == hsh2 ? nil : hsh2
-#--}}}
+      hsh2 = reject(&block)
+      self == hsh2 ? nil : hsh2
     end
     def replace hsh2
-#--{{{
-        @order = hsh2.keys 
-        super hsh2
-#--}}}
+      @order = hsh2.keys 
+      super hsh2
     end
     def shift
-#--{{{
-        key = @order.first
-        key ? [key,delete(key)] : super
-#--}}}
+      key = @order.first
+      key ? [key,delete(key)] : super
     end
     def unshift k,v
-#--{{{
-        unless self.include? k
-            @order.unshift k
-            orig_store(k,v)
-            true
-        else
-            false
-        end
-#--}}}
+      unless self.include? k
+        @order.unshift k
+        orig_store(k,v)
+        true
+      else
+        false
+      end
     end
     def push k,v
-#--{{{
-        unless self.include? k
-            @order.push k
-            orig_store(k,v)
-            true
-        else
-            false
-        end
-#--}}}
+      unless self.include? k
+        @order.push k
+        orig_store(k,v)
+        true
+      else
+        false
+      end
     end
     def pop
-#--{{{
-        key = @order.last
-        key ? [key,delete(key)] : nil
-#--}}}
+      key = @order.last
+      key ? [key,delete(key)] : nil
     end
     def to_a
-#--{{{
-        ary = []
-        each { |k,v| ary << [k,v] }
-        ary
-#--}}}
+      ary = []
+      each { |k,v| ary << [k,v] }
+      ary
     end
     def to_s
-#--{{{
-        self.to_a.to_s
-#--}}}
+      self.to_a.to_s
     end
     def inspect
-#--{{{
-        ary = []
-        each {|k,v| ary << k.inspect + "=>" + v.inspect}
-        '{' + ary.join(", ") + '}'
-#--}}}
+      ary = []
+      each {|k,v| ary << k.inspect + "=>" + v.inspect}
+      '{' + ary.join(", ") + '}'
     end
     def update hsh2
-#--{{{
-        hsh2.each { |k,v| self[k] = v }
-        self
-#--}}}
+      hsh2.each { |k,v| self[k] = v }
+      self
     end
     alias :merge! update
     def merge(hsh2)
-#--{{{
-        self.dup.update(hsh2)
-#--}}}
+      self.dup.update(hsh2)
     end
     def select
-#--{{{
-        ary = []
-        each { |k,v| ary << [k,v] if yield k,v }
-        ary
-#--}}}
+      ary = []
+      each { |k,v| ary << [k,v] if yield k,v }
+      ary
     end
     def class
-#--{{{
       Hash
-#--}}}
     end
 
     attr_accessor "to_yaml_style"
@@ -220,10 +157,10 @@ class OrderedHash < ::Hash
         unless defined? @__yaml_inline_meth
           @__yaml_inline_meth =
             lambda {|opts|
-              YAML::quick_emit(object_id, opts) {|emitter|
-                emitter << '{ ' << map{|kv| kv.join ': '}.join(', ') << ' }'
-              }
+            YAML::quick_emit(object_id, opts) {|emitter|
+              emitter << '{ ' << map{|kv| kv.join ': '}.join(', ') << ' }'
             }
+          }
           class << self
             def to_yaml opts = {}
               begin
@@ -239,5 +176,5 @@ class OrderedHash < ::Hash
       @__yaml_inline = bool
     end
     def yaml_inline!() self.yaml_inline = true end
-#--}}}
-end # class OrderedHash
+  end # class OrderedHash
+end
