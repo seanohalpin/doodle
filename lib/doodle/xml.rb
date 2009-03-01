b8770cc5b2d7c6f09a8d093d77b42551e781977b
Sean O'Halpin <sean.ohalpin@gmail.com>
2009-03-01 21:58:18 +0000

    support REXML and Nokogiri (though latter disabled for moment)

--- lib/doodle/xml.rb
+++ lib/doodle/xml.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
-#require 'nokogiri'
+# some prob with different versions of libxml on different platforms
+# begin
+#   require 'nokogiri'
+# rescue LoadError
 require 'rexml/document'
+# end
 
 class Doodle
   module EscapeXML
@@ -27,30 +31,80 @@ class Doodle
       include Doodle::XML
     end
 
-    def self.parse_xml(xml)
-      #doc = Nokogiri::XML(str) # nokogiri
-      REXML::Document.new(xml)
+    # adapter module for REXML
+    module REXMLAdapter
+
+      # return the parsed xml DOM
+      def parse_xml(xml)
+        REXML::Document.new(xml)
+      end
+
+      # test whether a node is a text node
+      def text_node?(node)
+        node.kind_of?(::REXML::Text)
+      end
+
+      # get the first XML element in the document
+      def get_root(doc)
+        # skip :REXML::XMLDecl
+        # REXML children does not properly implement shift (or pop)
+        root = doc.children.find { |el, i| el.kind_of?(REXML::Element) }
+        if root.nil?
+          raise ArgumentError, "XML document does not contain any elements"
+        else
+          root
+        end
+      end
     end
 
-    def self.text_node?(node)
-      node.kind_of?(::REXML::Text)
-      # node.name == "text"
+    # adapter module for Nokogiri
+    module NokogiriAdapter
+
+      # return the parsed xml DOM
+      def parse_xml(xml)
+        Nokogiri::XML(xml)
+      end
+
+      # test whether a node is a text node
+      def text_node?(node)
+        node.name == "text"
+      end
+
+      # get the first XML element in the document
+      def get_root(doc)
+        doc.children.first
+      end
     end
 
-    def self.from_xml(ctx, str)
-      doc = parse_xml(str)
-      root = doc.children.first
-      from_xml_elem(ctx, root)
+    if Object.const_defined?(:Nokogiri)
+      extend NokogiriAdapter
+    else
+      extend REXMLAdapter
     end
 
-    def self.from_xml_elem(ctx, root)
-      attributes = root.attributes.inject({ }) { |hash, (k, v)| hash[k] = EscapeXML.unescape(v.to_s); hash}
-      text, children = root.children.partition{ |x| text_node?(x) }
-      text = text.map{ |x| x.to_s}.reject{ |s| s =~ /^\s*$/}.join('')
-      oroot = Utils.const_lookup(root.name, ctx).new(text, attributes) {
-        from_xml_elem(root)
-      }
-      oroot
+    class << self
+      # parse XML +str+ into a Doodle object graph, using +ctx+ as the
+      # root namespace (can be module or class)
+      #
+      # this is the entry point - most of the heavy lifting is done by
+      # +from_xml_elem+
+      def from_xml(ctx, str)
+        doc = parse_xml(str)
+        root = get_root(doc)
+        from_xml_elem(ctx, root)
+      end
+
+      # helper function to handle recursion
+      def from_xml_elem(ctx, root)
+        attributes = root.attributes.inject({ }) { |hash, (k, v)| hash[k] = EscapeXML.unescape(v.to_s); hash}
+        text, children = root.children.partition{ |x| text_node?(x) }
+        text = text.map{ |x| x.to_s}.reject{ |s| s =~ /^\s*$/}.join('')
+        oroot = Utils.const_lookup(root.name, ctx).new(text, attributes) {
+          from_xml_elem(root)
+        }
+        oroot
+      end
+      private :from_xml_elem
     end
 
     def from_xml_elem(parent)
@@ -67,12 +121,16 @@ class Doodle
       #parent
       self
     end
+    private :from_xml_elem
 
+    # override this to define a tag name for output - the default is
+    # to use the classname (wthout namespacing)
     def tag
       #self.class.to_s.split(/::/)[-1].downcase
       self.class.to_s.split(/::/)[-1]
     end
 
+    # override this to define a specialised attributes format
     def format_attributes(attributes)
       if attributes.size > 0
         " " + attributes.map{ |k, v| %[#{ k }="#{ v }"]}.join(" ")
@@ -81,6 +139,7 @@ class Doodle
       end
     end
 
+    # override this to define a specialised tag format
     def format_tag(tag, attributes, body)
       if body.size > 0
         ["<#{tag}#{format_attributes(attributes)}>", body, "</#{tag}>"]
@@ -89,6 +148,7 @@ class Doodle
       end.join('')
     end
 
+    # output Doodle object graph as xml
     def to_xml
       body = []
       attributes = []
