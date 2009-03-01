9221f983e0f179c9f2377f02dca166068fa31d98
Sean O'Halpin <sean.ohalpin@gmail.com>
2009-03-01 21:53:17 +0000

    added xml example (more in docs)

--- examples/xml1.rb
+++ examples/xml1.rb
@@ -1,13 +1,18 @@
 # RUBYLIB=lib xmpfilter
-
+#: requires
+require 'rubygems'
 require 'doodle'
 require 'doodle/xml'
 
+#: source
 xml_source = %[<Address where="home"><City>London<Country>UK</Country></City></Address>]
 
+#: include
 class Base < Doodle
   include Doodle::XML
 end
+
+#: definition
 class Country < Base
   has :_text_
 end
@@ -20,15 +25,18 @@ class Address < Base
   has City
 end
 
+#: use
 a = Address :where => 'home' do
   city "London", :country => "England" do
     country "UK"
   end
 end
 
-a                               # => 
-a.to_xml == xml_source          # => 
+#: output
+a                               # => #<Address:0x114c534 @city=#<City:0x1148830 @country=#<Country:0x113fa00 @_text_="UK">, @_text_="London">, @where="home">
+a.to_xml == xml_source          # => true
+#: load
 b = Doodle::XML.from_xml(Base, xml_source)
-b                               # => 
-b == a                          # => 
-puts a.to_xml
+b                               # => #<Address:0x113152c @city=#<City:0x112e304 @country=#<Country:0x112b8e8 @_text_="UK">, @_text_="London">, @where="home">
+#: equality
+b == a                          # => true
