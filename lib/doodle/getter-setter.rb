b7c60fc141404bebde1f98ec1ecdbf7445ea7b4d
Sean O'Halpin <sean.ohalpin@gmail.com>
2009-03-22 14:48:59 +0000

    try conversion before trying class.new

--- lib/doodle/getter-setter.rb
+++ lib/doodle/getter-setter.rb
@@ -88,49 +88,75 @@ class Doodle
     # set an attribute by name - apply validation if defined
     # FIXME: move
     def _setter(name, *args, &block)
-      Doodle::Debug.d { [:_setter, name, args] }
+      Doodle::Debug.d { [:_setter, 1, name, args] }
       #p [:_setter, name, *args]
       att = __doodle__.lookup_attribute(name)
+      Doodle::Debug.d { [:_setter, 2, :att, att] }
       if att && __doodle__.validation_on && att.readonly
+        Doodle::Debug.d { [:_setter, 3] }
         raise Doodle::ReadOnlyError, "Trying to set a readonly attribute: #{att.name}", Doodle::Utils.doodle_caller
       end
       if block_given?
+        Doodle::Debug.d { [:_setter, 4] }
         # if a class has been defined, let's assume it can take a
         # block initializer (test that it's a Doodle or Proc)
         if att.kind && !att.abstract && klass = att.kind.first
+          Doodle::Debug.d { [:_setter, 5] }
           if [Doodle, Proc].any?{ |c| klass <= c }
             # p [:_setter, '# 1 converting arg to value with kind ' + klass.to_s]
+            Doodle::Debug.d { [:_setter, 6] }
             args = [klass.new(*args, &block)]
           else
+            Doodle::Debug.d { [:_setter, 7] }
             __doodle__.handle_error att.name, ArgumentError, "#{klass} #{att.name} does not take a block initializer", Doodle::Utils.doodle_caller
           end
         else
+          Doodle::Debug.d { [:_setter, 8] }
           # this is used by init do ... block
           args.unshift(DeferredBlock.new(block))
         end
       end
-      if att # = __doodle__.lookup_attribute(name)
+      Doodle::Debug.d { [:_setter, 9] }
+      if att
+        Doodle::Debug.d { [:_setter, 10] }
         if att.kind && !att.abstract && klass = att.kind.first
+          Doodle::Debug.d { [:_setter, 11, :att_kind, klass] }
           if !args.first.kind_of?(klass) && [Doodle].any?{ |c| klass <= c }
+            Doodle::Debug.d { [:_setter, 12] }
             #p [:_setter, "#2 converting arg #{att.name} to value with kind #{klass.to_s}"]
             #p [:_setter, args]
             begin
-              args = [klass.new(*args, &block)]
+              # try conversion if only one argument and conversion exists
+              conversion = att.__doodle__.conversions[args.first.class]
+              Doodle::Debug.d { [:_setter, 12.1, name, att.__doodle__.conversions.keys, conversion, args.first, args.first.class ] }
+              if args.size == 1 && conversion
+                Doodle::Debug.d { [:_setter, 12.2, :trying_conversion ] }
+                args = att.convert(self, *args)
+              else
+                Doodle::Debug.d { [:_setter, 12.3, :initialize_using_first_kind, klass, 13] }
+                args = [klass.new(*args, &block)]
+              end
             rescue Object => e
+              Doodle::Debug.d { [:_setter, 14] }
               __doodle__.handle_error att.name, e.class, e.to_s, Doodle::Utils.doodle_caller
             end
           end
+        else
+          Doodle::Debug.d { [:_setter, 10.1, :abstract, att.abstract] }
         end
+        Doodle::Debug.d { [:_setter, 15] }
         #p [:_setter, :got_att1, name, args.map{ |x| x.object_id }, *args]
         v = ivar_set(name, att.validate(self, *args))
 
         #p [:_setter, :got_att2, name, ivar, :value, v]
         #v = instance_variable_set(ivar, *args)
       else
+        Doodle::Debug.d { [:_setter, 16] }
         #p [:_setter, :no_att, name, *args]
         ##DBG: Doodle::Debug.d { [:_setter, "no attribute"] }
         v = ivar_set(name, *args)
       end
+      Doodle::Debug.d { [:_setter, 17, v] }
       validate!(false)
       v
     end
@@ -141,9 +167,18 @@ class Doodle
       # fixme: move
       def define_getter_setter(name, params = { }, &block)
         # need to use string eval because passing block
-        sc_eval "def #{name}(*args, &block); getter_setter(:#{name}, *args, &block); end", __FILE__, __LINE__
-        sc_eval "def #{name}=(*args, &block); _setter(:#{name}, *args); end", __FILE__, __LINE__
-
+        sc_eval <<-EOT, __FILE__, __LINE__
+          def #{name}(*args, &block)
+            Doodle::Debug.d { [:SETTER, :#{name}, args, block ] }
+            getter_setter(:#{name}, *args, &block)
+          end
+        EOT
+        sc_eval <<-EOT, __FILE__, __LINE__
+          def #{name}=(*args, &block)
+            Doodle::Debug.d { [:SETTER, :#{name}=, args ] }
+            _setter(:#{name}, *args)
+          end
+        EOT
         # this is how it should be done (in 1.9)
         #       module_eval {
         #         define_method name do |*args, &block|
