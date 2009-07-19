890bf7c118a0aff7f166b5adc0bc7ec9aae68a3f
Sean O'Halpin <sean.ohalpin@gmail.com>
2009-07-19 21:50:37 +0100

    revised equality so that doodles can be used as hash keys

--- lib/doodle/equality.rb
+++ lib/doodle/equality.rb
@@ -1,23 +1,21 @@
 class Doodle
-  # two doodles of the same class with the same attribute values are
-  # considered equal
   module Equality
+    # two doodles of the same class with the same attribute values are
+    # considered equal
     def eql?(o)
-      #       p [:comparing, self.class, o.class, self.class == o.class]
-      #       p [:values, self.doodle.values, o.doodle.values, self.doodle.values == o.doodle.values]
-      #       p [:attributes, doodle.attributes.map { |k, a| [k, send(k).==(o.send(k))] }]
-      res = self.class == o.class &&
-        #self.doodle.values == o.doodle.values
-        # short circuit comparison
-        doodle.attributes.all? { |k, a| send(k).==(o.send(k)) }
-      #       p [:res, res]
-      res
+      o.kind_of?(Doodle::Core) &&
+        self.class == o.class &&
+        doodle.key_values_without_defaults.all? { |k, v| o.respond_to?(k) && v.eql?(o.send(k)) }
+      #         [self.class, doodle.key_values_without_defaults].eql?([o.class, o.doodle.key_values_without_defaults])
     end
     def ==(o)
-      eql?(o)
+      o.kind_of?(Doodle::Core) &&
+        self.class == o.class &&
+        doodle.key_values_without_defaults.all? { |k, v| o.respond_to?(k) && v == o.send(k) }
+      #        [self.class, doodle.key_values_without_defaults] == [o.class, o.doodle.key_values_without_defaults]
     end
     def hash
-      doodle.key_values_without_defaults.hash
+      [self.class, doodle.key_values_without_defaults].hash
     end
   end
 end
